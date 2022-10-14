ed2fcbf3813205f6e9ddcdf71977306e3de05482
Ondrej Sika <ondrej@ondrejsika.com>
2022-10-14 13:31:19 +0200

    [template] feat: Add normalize.css & gray background
    (cherry picked from commit 1de1b281f38e76e63629a94aacdbbe578d92e9d9)

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "normalize.css";
 
-let Index = () => {
+const style = {
+  backgroundColor: "grey",
+  height: "100vh",
+  margin: 0,
+  padding: 10,
+};
+
+const Index = () => {
   return (
-    <body>
+    <div style={style}>
       <h1>Super Cool Dentsu Project</h1>
       <p>
         This is a super cool project that Dentsu is working on. It's going to be
         legendary.
       </p>
-    </body>
+    </div>
   );
 };
 
