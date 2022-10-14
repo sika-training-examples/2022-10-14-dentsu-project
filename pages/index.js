97299f06e1e08df0a5c55ced955122371e24c648
Ondrej Sika <ondrej@ondrejsika.com>
2022-10-14 13:15:56 +0200

    content: Add some text

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ let Index = () => {
   return (
     <body>
       <h1>Super Cool Dentsu Project</h1>
+      <p>
+        This is a super cool project that Dentsu is working on. It's going to be
+        legendary.
+      </p>
     </body>
   );
 };
